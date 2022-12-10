758bbc3d2b0223606ac1b1a2136718777bbf3a9f
edwinkapkei <dev.kapkei@gmail.com>
2022-12-10 15:38:50 +0300

    toolbar and back button implementation

--- app/src/main/java/com/edwinkapkei/githubapi/views/MainActivity.kt
+++ app/src/main/java/com/edwinkapkei/githubapi/views/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.edwinkapkei.githubapi.R
 import com.edwinkapkei.githubapi.databinding.ActivityMainBinding
 import com.edwinkapkei.githubapi.views.viewmodel.UserViewModel
@@ -26,7 +28,9 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration)
 
-        viewModel = ViewModelProvider(this,factory)[UserViewModel::class.java]
+        viewModel = ViewModelProvider(this, factory)[UserViewModel::class.java]
     }
 }
